9292db24de0ae7d8efede73e5740596c2eead638
noranl <281170419@qq.com>
2019-10-06 19:55:02 +0800

    improve utils

--- web/utils/js/main.js
+++ web/utils/js/main.js
@@ -1,10 +1,25 @@
-(function(){
+(function () {
     var app = new Vue({
         el: '#app',
         data: {
             input1: 'Hello Vue!',
             output1: 'Hello Vue!',
-            pageReady: false
+            pageReady: false,
+            separatorOptions: {
+                row: '按行分隔',
+                space: '按空格分隔',
+                custom: '自定义',
+            },
+            strForm: {
+                customSeparator: ',',
+                separator: null,
+                includes: '","',
+                out: {
+                    separator: null,
+                    customSeparator: '',
+                    includes: '","',
+                }
+            }
         },
         mounted: function () {
             var that = this;
@@ -13,16 +28,47 @@
             }, 1000);
         },
         methods: {
-            formatInput1: function(){
-                var s = '';
-                console.log(this.input1.split('\n'));
+            formatInput1: function () {
+                var s = this.input1;
                 var sArr = [];
-                this.input1.split('\n').forEach(function(item){
-                    if(item.length > 0){
-                        sArr.push('"' + item + '"');
+                var splitSeparator = '';
+                var joinSeparator = '';
+                var strForm = this.strForm;
+                var separator = strForm.separator;
+                var outSeparator = strForm.out.separator;
+                var inIncludeArr = strForm.includes.split(',');
+                var outIncludeArr = strForm.out.includes.split(',');
+
+                if (outSeparator) {
+                    switch (outSeparator) {
+                        case 'row': joinSeparator = '\n'; break;
+                        case 'space': joinSeparator = ' '; break;
+                        case 'custom': joinSeparator = strForm.out.customSeparator; break;
                     }
-                });
-                return sArr.join(',');
+                }
+
+                if (separator) {
+                    switch (strForm.separator) {
+                        case 'row': splitSeparator = '\n'; break;
+                        case 'space': splitSeparator = ' '; break;
+                        case 'custom': splitSeparator = strForm.customSeparator; break;
+                    }
+
+                    this.input1.split(splitSeparator).forEach(function (item) {
+                        if(inIncludeArr.length > 1){
+                            sArr.push(item.replace(inIncludeArr[0], outIncludeArr[0]).replace(inIncludeArr[1], outIncludeArr[1]));
+                        }else{
+                            sArr.push(outIncludeArr[0] + item + outIncludeArr[1]);
+                        }
+                    });
+
+                    if (sArr.length > 1 && sArr[sArr.length - 1].replace(new RegExp('^' + outIncludeArr[0] + '(.*)' + outIncludeArr[1] + '$', 'ig'), '$1').length < 1) {
+                        console.log(sArr[sArr.length - 1].replace(new RegExp('^' + outIncludeArr[0] + '(.*)' + outIncludeArr[1] + '$', 'ig'), '$1'));
+                        sArr.pop();
+                    }
+                    s = sArr.join(joinSeparator);
+                }
+                return s;
             }
         }
     })
